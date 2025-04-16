afa5218a660426cef61c1d33a0ce08b9ff7d46d1
Hector <61565698+hectorfd@users.noreply.github.com>
2025-04-16 11:32:43 -0500

    variables

---
+++ sección-2/tipe_data.py
@@ -0,0 +1,44 @@
+# types of variables
+
+#int
+number = 5
+temperature = -34
+#string
+name = 'Hector'
+last_name = "Ferro"
+#list
+list = [1, 2, 3, 4, 5]
+names = ['Hector', 'Ferro', 'Davalos']
+mixed_list = [1, 'Hector', 5.5, True]
+#tuple
+tuple = (1, 2, 3, 4, 5)
+names_tuple = ('Hector', 'Ferro', 'Davalos')
+mixed_tuple = (1, 'Hector', 5.5, True)
+#dict
+dict = {'name': 'Hector', 'age': 31}
+dict2 = {'name': 'Hector', 'age': 31, 'hobbies': ['programming', 'reading']}
+dict3 = {'name': 'Hector', 'age': 31, 'hobbies': ['programming', 'reading'], 'address': {'city': 'Madrid', 'country': 'Spain'}}
+#set
+set = {1, 2, 3, 4, 5}
+names_set = {'Hector', 'Ferro', 'Davalos'}
+#bool
+bool = True
+es_menor = False
+#float
+float = 5.5
+float2 = -5.5
+#complex
+complex = 5 + 3j
+#None
+none = None
+
+print("Integer:", number, temperature)
+print("String:", name, last_name)
+print("List:", list, names, mixed_list)
+print("Tuple:", tuple, names_tuple, mixed_tuple)
+print("Dictionary:", dict, dict2, dict3)
+print("Set:", set, names_set)
+print("Boolean:", bool, es_menor)
+print("Float:", float, float2)
+print("Complex:", complex)
+print("None:", none)
