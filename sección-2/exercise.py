923950327f593149f4be233a66735f62f813d34f
Hector <61565698+hectorfd@users.noreply.github.com>
2025-04-16 13:25:37 -0500

    upgrade code generator

--- sección-2/exercise.py
+++ sección-2/exercise.py
@@ -8,7 +8,11 @@ year_of_birth = int(input('Enter your year of birth in format(yyyy)please:'))
 def generate_code(name, last_name, year_of_birth):
     #* code generator
     code = f"{name[:2].upper()}{last_name[:2].upper()}{str(year_of_birth)[:2]}"
-    return code
+    #* four random numbers
+    import random
+    numbers = str(random.randint(1000, 9999))
+    generator = code + numbers
+    return generator
 
 # print
-print(f"Your code is: {generate_code(name, last_name, year_of_birth)}") 
+print(f"Your ID is: {generate_code(name, last_name, year_of_birth)}") 
