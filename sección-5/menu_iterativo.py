ee3073a662050cb27b70596e05e0fa4e07edcaec
Hector <61565698+hectorfd@users.noreply.github.com>
2025-04-18 13:24:46 -0500

    update menu iterativo

--- sección-5/menu_iterativo.py
+++ sección-5/menu_iterativo.py
@@ -1,16 +1,21 @@
 espacio = 4
 print('Sistem of administrator')
-print('Menu:')
-print(" "* espacio, '1. Add user')
-print(" "* espacio, '2. Delete user')
-print(" "* espacio, '3. exit')
+def menu():
+    print('Menu:')
+    print(" "* espacio, '1. Add user')
+    print(" "* espacio, '2. Delete user')
+    print(" "* espacio, '3. exit')
+
+menu()
 
 while True:
     value = int(input('Select an option: '))
     if value == 1:
         print('Add user...')
+        menu()
     elif value == 2:
         print('Delete user...')
+        menu()
     elif value == 3:
         print('Exit...')
         break
