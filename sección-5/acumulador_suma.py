c3cdeb58ae618169d437fa67312fc67f2adda0a8
Hector <61565698+hectorfd@users.noreply.github.com>
2025-04-18 13:17:14 -0500

    acumulador suma

---
+++ sección-5/acumulador_suma.py
@@ -0,0 +1,10 @@
+count = 1
+for i in range(5):
+    count += i
+    suma = count + i
+print(suma)
+# 1 iteracion -> count = 1 + 0 = 1 -> suma = 1 + 0 = 1
+# 2 iteracion -> count = 1 + 1 = 2 -> suma = 2 + 1 = 3
+# 3 iteracion -> count = 2 + 2 = 4 -> suma = 4 + 2 = 6
+# 4 iteracion -> count = 4 + 3 = 7 -> suma = 7 + 3 = 10
+# 5 iteracion -> count = 7 + 4 = 11 -> suma = 11 + 4 = 15
